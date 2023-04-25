5d99050b13b90b5fdfa3ea1eda1783a83c31fca4
Deependra-yadav <poojayadav.pp1@gmail.com>
2023-04-25 18:25:13 +0530

    scripting

---
+++ app.js
@@ -0,0 +1,8 @@
+//console.log("console is working");
+// let  question=prompt("let me know your name");
+// alert("this is working fine " + question);
+const btnTranslate = document.getElementById("btn-translate");
+btnTranslate.addEventListener("click", myfunction);
+function myfunction(){
+    document.getElementById("demo").innerHTML="hello there";
+}
