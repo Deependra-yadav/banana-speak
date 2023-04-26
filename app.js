e3628fa529484c6644063a887155dd50a78d1b84
Deependra-yadav <poojayadav.pp1@gmail.com>
2023-04-26 15:30:59 +0530

    taking input and console

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 //console.log("console is working");
 // let  question=prompt("let me know your name");
 // alert("this is working fine " + question);
-const btnTranslate = document.getElementById("btn-translate");
-btnTranslate.addEventListener("click", myfunction);
-function myfunction(){
-    document.getElementById("demo").innerHTML="hello there";
-}
+// var txtInput = document.querySelector("#txt-input"); 
+// const btnTranslate = document.getElementById("btn-translate");
+// btnTranslate.addEventListener("click", function myfunction(){
+//    console.log("btn clicked");
+//    console.log("input".txtInput.value);
+// //    document.getElementById("demo").innerHTML="hello there";
+// });
+// // function myfunction(){
+// //     document.getElementById("demo").innerHTML="hello there";
+// // }
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput  = document.querySelector("#txt-input");
+ // console.log(txtInput);
+   
+   function myfunction(){
+      console.log(txtInput.value);
+   } 
+   btnTranslate.addEventListener("click", myfunction);
