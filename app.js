1dd6d24e4f750cd34c9c4fd0e1f9e984315bf3e6
Deependra-yadav <poojayadav.pp1@gmail.com>
2023-04-29 16:04:00 +0530

    functionality : done

--- app.js
+++ app.js
@@ -15,9 +15,31 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput  = document.querySelector("#txt-input");
 var outputdiv = document.querySelector("#output");
  // console.log(txtInput);
-   
+
+   var serverURL =" https://api.funtranslations.com/translate/ferb-latin.json" 
+    function doFetch(text){
+      return serverURL + "?" + "text=" + text
+    }
+//error handiling//
+  function errorHandler(error){
+    console.log("error occured " + error )
+    alert("Something went wrong with server : please try again letter after some time")
+  }
+  
    function myfunction(){
-    //   console.log(txtInput.value);
-      outputdiv.innerText= "ida pinda hula " + txtInput.value;
-   } 
+      var inputText = txtInput.value;  // taking input
+
+      // calling server for processing
+      fetch(doFetch(inputText))
+        .then(response=>response.json())
+        .then(json => {
+          var translatedText = json.contents.translated;
+          outputdiv.innerText = translatedText ;
+        })
+        .catch(errorHandler)
+   }; 
    btnTranslate.addEventListener("click", myfunction);
+
+
+    //   console.log(txtInput.value);
+      //outputdiv.innerText= "ida pinda hula " + txtInput.value;
